893f556c719c872f3e422f8f4daac16dd993b63e
githings <63792542+githings@users.noreply.github.com>
2025-04-29 21:15:36 +0900

    Title: 행복한 수열의 개수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250429/행복한 수열의 개수/number-of-happy-sequence.c
+++ 250429/행복한 수열의 개수/number-of-happy-sequence.c
@@ -14,28 +14,48 @@ int main(){
     }
 
 
-    for (int i = 0;  i< n; i++){
-        int sameCount=1;
-        for (int j=0; j<n-1; j++) {
-            if (grid[i][j]==grid[i][j+1]) sameCount++;
-            else if (sameCount>=m) {
-                break;
-            }
-            else sameCount=1;
-        }
-        if (sameCount>=m) ans++;
-    }
+    // for (int i = 0;  i< n; i++){
+    //     int sameCount=1;
+    //     for (int j=0; j<n-1; j++) {
+    //         if (grid[i][j]==grid[i][j+1]) sameCount++;
+    //         else if (sameCount>=m) {
+    //             break;
+    //         }
+    //         else sameCount=1;
+    //     }
+    //     if (sameCount>=m) ans++;
+    // }
 
-    for (int i = 0;  i< n; i++){
-        int sameCount=1;
+    // for (int i = 0;  i< n; i++){
+    //     int sameCount=1;
+    //     for (int j=0; j<n-1; j++) {
+    //         if (grid[j][i]==grid[j+1][i]) sameCount++;
+    //         else if (sameCount>=m) {
+    //             break;
+    //         }
+    //         else sameCount=1;
+    //     }
+    //     if (sameCount>=m) ans++;
+    // }
+
+     for (int i = 0;  i< n; i++){
+        int sameCountRow=1, sameCountColumn=1, rowFlag=0, colFlag=0;
         for (int j=0; j<n-1; j++) {
-            if (grid[j][i]==grid[j+1][i]) sameCount++;
-            else if (sameCount>=m) {
-                break;
+            if (sameCountRow>=m) rowFlag=1;
+            else if (grid[i][j]==grid[i][j+1]) {
+                sameCountRow++;
             }
-            else sameCount=1;
+            else sameCountRow=1;
+
+            if (sameCountColumn>=m) colFlag=1;
+            else if (grid[j][i]==grid[j+1][i]) {
+                sameCountColumn++;
+            }
+            else sameCountColumn=1;
         }
-        if (sameCount>=m) ans++;
+        if (sameCountRow>=m) rowFlag=1;
+        if (sameCountColumn>=m) colFlag=1;
+        ans += (rowFlag+colFlag);
     }
 
 
