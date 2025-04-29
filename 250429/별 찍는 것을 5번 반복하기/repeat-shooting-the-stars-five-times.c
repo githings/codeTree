75c6a1839402cc472f1b9134402593c9cb62196f
githings <63792542+githings@users.noreply.github.com>
2025-04-29 19:30:40 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250429/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void Print5Stars() {
+    printf("**********\n");
+}
+
+
+int main() {
+    // Please write your code here.
+    for (int i=0;i<5;i++) Print5Stars();
+    return 0;
+}
