a4bbad2c91d1b4774414c3a973b2ffc7d1b2ff52
githings <63792542+githings@users.noreply.github.com>
2025-04-29 19:51:01 +0900

    Title: 최고의 33위치, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250429/최고의 33위치/best-place-of-33.c
+++ 250429/최고의 33위치/best-place-of-33.c
@@ -1,18 +1,60 @@
 #include <stdio.h>
 
 int grid[20][20];
-int n,k;
+int n,m;
 
 int main() {
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             scanf("%d", &grid[i][j]);
-            k=grid[i][j];
         }
     }
     
     // Please write your code here.
-    printf("%d", k);    
+
+
+    for (int i=0;i<=n-3;i++) {
+        for (int j=0;j<=n-3;j++) {
+            int s=0;
+            for (int k=i; k<i+3; k++) {
+                for (int l=j; l<j+3; l++){
+                    s+=grid[k][l];
+                    // printf("%d#", grid[k][l]);
+                }
+            }
+            if (s>m) m=s;
+        }
+    }
+
+    //         s += grid
+    //         grid[i][j] +
+    //         grid[i][j+1] +
+    //         grid[i][j+2] +
+
+    //         grid[i+1][j] +
+    //         grid[i+1][j+1] +
+    //         grid[i+1][j+2] +
+
+    //         grid[i+2][j] +
+    //         grid[i+2][j+1] +
+    //         grid[i+2][j+2] +
+    //     }
+    // }
+
+
+    // for (;i<n-3;i++) {
+    //     for (;j<n-3;j++) {
+    //         for (int k=i; k<i+3; k++) {
+    //             for (int l=j)
+    //         }
+    //     }
+    // }
+
+
+    
+    // Please write your code here.
+    printf("%d", m);    
+    
     return 0;
 }
