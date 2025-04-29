e3ae17629d6e8d71648392925e3c443a32888c85
githings <63792542+githings@users.noreply.github.com>
2025-04-29 20:33:37 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250429/행복한 수열의 개수/number-of-happy-sequence.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int n, m, ans;
+int grid[101][101];
+
+int main(){
+    scanf("%d %d", &n, &m);
+
+    if (m==1) {printf("%d", n*2); return 0;}
+    for (int i = 0; i < n; i++){
+        int j;
+        for (j=0; j < m; j++){
+            scanf("%d", &grid[i][0]);
+        }
+
+        // int sameCount=0;
+        // for (; j < n; j++){
+        //     scanf("%d", &grid[i][j]);
+        //     while ((grid[i][j-1] != grid[i][j]) ||
+        //             ( j-1 >0) ||
+        //             (sameCount <m)) {
+        //                 sameCount++;
+        //             }
+        //     if (sameCount>=m){
+        //         ans++;
+        //         while(getchar()!='\n');
+        //         continue;
+        //     }
+        // }
+    }
+
+
+    for (int i = 0;  i< n; i++){
+        int sameCount=0, j=0;
+        for (int j=0; j<n-1; j++) {
+            // while (n-j>=m)
+            if (grid[i][j]==grid[i][j+1]) sameCount++;
+            else sameCount=0;
+        }
+        if (sameCount>=m) ans++;
+    }
+
+    for (int j = 0;  j< n; j++){
+        int sameCount=0, i=0;
+        for (int i=0;i <n-1; i++) {
+            // while (n-j>=m)
+            if (grid[i][j]==grid[i][j+1]) sameCount++;
+            else sameCount=0;
+        }
+        if (sameCount>=m) ans++;
+    }
+
+
+    // Please write your code here.
+    printf("%d", ans);
+    return 0;
+}
