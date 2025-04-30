c37249fc0440c22e56b4b2d1932c58af0373eb9e
githings <63792542+githings@users.noreply.github.com>
2025-04-30 12:17:11 +0900

    Title: 트로미노, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250430/트로미노/tromino.c
+++ 250430/트로미노/tromino.c
@@ -15,40 +15,17 @@ int main() {
     // // Please write your code here.
     int globalSum=0;
     int localSum = 0;
-    // //수평선 하나 
-    // if (j+2<n) {
-    //     localSum = g[i][j] + g[i][j+1] + g[i][j+2]
-    // }
-    // if (globalSum<localSum) globalSum = localSum;
-    // //수직선 하나
-    // if (i+2<n) {
-    //     localSum = g[i][j] + g[i+1][j] + g[i+2][j]
-    // }
-    // if (globalSum<localSum) globalSum = localSum;
-
-    // // 기본 방향
-    // if (i-1>=0 && j+1<n) localSum = g[i][j] + g[i-1][j] + g[i][j+1]
-    // if (globalSum<localSum) globalSum = localSum;
-    // // 90도
-    // if (i+1<n && j+1<n) localSum = g[i][j] + g[i][j+1] + g[i+1][j]
-    // if (globalSum<localSum) globalSum = localSum;
-    // //180도
-    // if (i+1<n && j-1>=0) localSum = g[i][j] + g[i][j-1] + g[i+1][j]
-    // if (globalSum<localSum) globalSum = localSum;
-    // //270도
-    // if (i-1>=0 && j-1>=0) localSum = g[i][j] + g[i][j-1] + g[i-1][j]
-    // if (globalSum<localSum) globalSum = localSum;
-
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             //수평선 하나 
-            if (j+2<n) {
+            if (j+2<=n) {
                 localSum = g[i][j] + g[i][j+1] + g[i][j+2];
+                // printf("%d %d \n", globalSum, localSum);
             }
             if (globalSum<localSum) globalSum = localSum;
             //수직선 하나
-            if (i+2<n) {
+            if (i+2<=n) {
                 localSum = g[i][j] + g[i+1][j] + g[i+2][j];
             }
             if (globalSum<localSum) globalSum = localSum;
