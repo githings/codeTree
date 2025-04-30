cfde0309bf15f573ad4d20da4172873b26f22f13
githings <63792542+githings@users.noreply.github.com>
2025-04-30 12:30:51 +0900

    Title: 트로미노, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250430/트로미노/tromino.c
+++ 250430/트로미노/tromino.c
@@ -3,6 +3,8 @@
 
 int n, m;
 int g[MAX_SIZE][MAX_SIZE];
+int globalSum;
+int localSum;
 
 int main() {
     scanf("%d %d", &n, &m);
@@ -13,23 +15,16 @@ int main() {
     }
     
     // // Please write your code here.
-    int globalSum=0;
-    int localSum = 0;
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             //수평선 하나 
-            if (j+2<=m) {
-                localSum = g[i][j] + g[i][j+1] + g[i][j+2];
-                // printf("%d %d \n", globalSum, localSum);
-            }
+            if (j+2<=m) localSum = g[i][j] + g[i][j+1] + g[i][j+2];
             if (globalSum<localSum) globalSum = localSum;
             //수직선 하나
-            if (i+2<=n) {
-                localSum = g[i][j] + g[i+1][j] + g[i+2][j];
-            }
+            if (i+2<=n) localSum = g[i][j] + g[i+1][j] + g[i+2][j];
             if (globalSum<localSum) globalSum = localSum;
-
+            
             // 기본 방향
             if (i-1>=0 && j+1<=m) localSum = g[i][j] + g[i-1][j] + g[i][j+1];
             if (globalSum<localSum) globalSum = localSum;
