2e2161b9abb47c0604ea60af7aaec7b056659b54
githings <63792542+githings@users.noreply.github.com>
2025-05-01 22:24:57 +0900

    Title: 금 채굴하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250501/금 채굴하기/gold-mining.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+// 사이즈, 금가격
+int n, m;
+int g[20][20];
+
+int main() {
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &g[i][j]);
+        }
+    }
+    
+    // Please write your code here.
+    int global_sum=0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int step_sum=0;
+            step_sum += g[i][j];
+            for (int s=0; s<n; s++) {
+                int x=j, y=i-s;
+                //first plane
+                while(y!=i && 0<=y && y<n && 0<=x && x<n) {
+                    step_sum += g[y][x];
+                    y += 1;
+                    x += 1;
+                }
+                //second plane
+                x=j+s, y=i;
+                while(x!=j && 0<=y && y<n && 0<=x && x<n) {
+                    step_sum += g[y][x];
+                    y += 1;
+                    x -= 1;
+                }
+                //third plane
+                x=j, y=i+s;
+                while(y!=i && 0<=y && y<n && 0<=x && x<n) {
+                    step_sum += g[y][x];
+                    y -= 1;
+                    x -= 1;
+                }
+                //forth plane
+                x=j-s, y=i;
+                while(x!=j && 0<=y && y<n && 0<=x && x<n) {
+                    step_sum += g[y][x];
+                    y -= 1;
+                    x += 1;
+                }
+                int cost = s*s + (s+1)*(s+1);
+                // 이득이라면 
+                // printf("[%d][%d]%d=%d\n", i,j,s,step_sum);
+                if ((step_sum*m - cost>=0) && global_sum<step_sum) global_sum = step_sum;
+                // if (global_sum<step_sum) global_sum = step_sum;
+            }
+        }
+    }
+
+    printf("%d", global_sum);
+
+
+    
+    return 0;
+}
