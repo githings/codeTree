b5540079b6c18f123b10a314a61760ef4f93075b
githings <63792542+githings@users.noreply.github.com>
2025-05-01 23:03:09 +0900

    Title: 금 채굴하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250501/금 채굴하기/gold-mining.c
+++ 250501/금 채굴하기/gold-mining.c
@@ -22,61 +22,37 @@ int main() {
             for (int s=0; s<=n; s++) {
                 int x=j, y=i-s;
                 //first plane
-                // while(0<=y && y<n && 0<=x && x<n) { y+=1; x+=1;}
                 while(y!=i) {
                     y += 1;
                     x += 1;
-                    if (!(0<=y && y<n && 0<=x && x<n)) {
-                        // y += 1;
-                        // x += 1;
-                        continue;
-                    }
+                    if (!(0<=y && y<n && 0<=x && x<n)) continue;
                     step_sum += g[y][x];
                 }
                 //second plane
-                x=j+s, y=i;
-                // while(0<=y && y<n && 0<=x && x<n) { y+=1; x-=1;}
                 while(x!=j) {
                     y += 1;
                     x -= 1;
-                    if (!(0<=y && y<n && 0<=x && x<n)) {
-                        // y += 1;
-                        // x -= 1;
-                        continue;
-                    }
+                    if (!(0<=y && y<n && 0<=x && x<n)) continue;
                     step_sum += g[y][x];
                 }
                 //third plane
-                x=j, y=i+s;
-                // while(0<=y && y<n && 0<=x && x<n) { y-=1; x-=1;}
                 while(y!=i) {
                     y -= 1;
                     x -= 1;
-                    if (!(0<=y && y<n && 0<=x && x<n)) {
-                        // y -= 1;
-                        // x -= 1;
-                        continue;
-                    }
+                    if (!(0<=y && y<n && 0<=x && x<n)) continue;
                     step_sum += g[y][x];
                 }
                 //forth plane
-                x=j-s, y=i;
-                // while(0<=y && y<n && 0<=x && x<n) { y-=1; x+=1;}
                 while(x!=j) {
                     y -= 1;
                     x += 1;
-                    if (!(0<=y && y<n && 0<=x && x<n)) {
-                        // y -= 1;
-                        // x += 1;
-                        continue;
-                    }
+                    if (!(0<=y && y<n && 0<=x && x<n)) continue;
                     step_sum += g[y][x];
                 }
-                int cost = s*s + (s+1)*(s+1);
+                // int cost = s*s + (s+1)*(s+1);
                 // 이득이라면 
                 // printf("[%d][%d]%d=%d\n", i,j,s,step_sum);
-                if ((step_sum*m - cost>=0) && global_sum<step_sum) global_sum = step_sum;
-                // if (global_sum<step_sum) global_sum = step_sum;
+                if ((step_sum*m - (s*s + (s+1)*(s+1))>=0) && global_sum<step_sum) global_sum = step_sum;
             }
         }
     }
